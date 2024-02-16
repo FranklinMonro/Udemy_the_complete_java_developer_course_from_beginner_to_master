bb8a1976eacd466d8ddfd4d15967c5495add5909
Franco Groenewald <franco@assetcontrol.co.za>
2024-02-16 07:32:15 +0200

    new section

---
+++ Udemy_Java_Classes_Objects/src/udemy_java_classes_objects/Udemy_Java_Classes_Objects.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package udemy_java_classes_objects;
+
+/**
+ *
+ * @author FrancoGroenewald
+ */
+public class Udemy_Java_Classes_Objects {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
